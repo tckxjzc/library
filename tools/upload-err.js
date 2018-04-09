1902d47d1dded1913dec299a06b909a66c514538
tckxjzc <tckxjzc@outlook.com>
2018-04-09 20:11:38 +0800

    change export

--- tools/upload-err.js
+++ tools/upload-err.js
@@ -1,4 +1,4 @@
 import Url from './url';
 export default (err)=>{
-    new Image().src=Url('https://www.tckxjzc.xyz/collect/upload').setParams({err:err,url:location.href}).url;
+    new Image().src=new Url('https://www.tckxjzc.xyz/collect/upload').setParams({err:err,url:location.href}).url;
 }
